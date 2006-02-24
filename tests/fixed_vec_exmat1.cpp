91f2e81adf3793d317593bf90f73cf39dee77665
dnave <demianmnave@users.noreply.github.com>
2006-02-24 02:07:02 +0000

    Added exmat and LTL tests.

---
+++ tests/fixed_vec_exmat1.cpp
@@ -0,0 +1,24 @@
+/* -*- C++ -*- ------------------------------------------------------------
+ @@COPYRIGHT@@
+ *-----------------------------------------------------------------------*/
+/** @file
+ *  @brief
+ */
+
+#include <iostream>
+#include <exmat.h>
+
+/* For convenience: */
+using std::cerr;
+using std::endl;
+using namespace exmat;
+
+typedef Vec<DenseColVecCon<double, 4> > vector_d4;
+#define VECINIT(_v_) _v_
+
+//#include "print_vector.cpp"
+#include "vector_algebra1.cpp"
+#include "vector_main1.cpp"
+
+// -------------------------------------------------------------------------
+// vim:ft=cpp
---
+++ tests/fixed_vec_ltl1.cpp
@@ -0,0 +1,26 @@
+/* -*- C++ -*- ------------------------------------------------------------
+ @@COPYRIGHT@@
+ *-----------------------------------------------------------------------*/
+/** @file
+ *  @brief
+ */
+
+#define LTL_TEMPLATE_LOOP_LIMIT 16
+
+#include <iostream>
+#include <ltl/fvector.h>
+
+/* For convenience: */
+using std::cerr;
+using std::endl;
+using namespace ltl;
+
+typedef FVector<double,4> vector_d4;
+#define VECINIT(_v_) _v_
+
+//#include "print_vector.cpp"
+#include "vector_algebra1.cpp"
+#include "vector_main1.cpp"
+
+// -------------------------------------------------------------------------
+// vim:ft=cpp
