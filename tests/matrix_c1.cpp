8ffa264dd6926e20cef4a2496540fba3cb8f1430
dnave <demianmnave@users.noreply.github.com>
2006-02-26 16:45:36 +0000

    Changed matrix_c1.cpp:timed1() to use references to matrix_d44.

--- tests/matrix_c1.cpp
+++ tests/matrix_c1.cpp
@@ -53,14 +53,14 @@ inline void mult(const matrix_d44 A, const matrix_d44 B, matrix_d44 res)
 
 /* The args should *not* be references: */
 inline void timed1(
-        matrix_d44 m,
-        const matrix_d44 m1,
-        const matrix_d44 m2,
-        const matrix_d44 m3,
+        matrix_d44& m,
+        const matrix_d44& m1,
+        const matrix_d44& m2,
+        const matrix_d44& m3,
         size_t n_iter
         )
 {
-    matrix_d44 tmp, tmp2; 
+    //matrix_d44 tmp, tmp2; 
     for(int i = 0; i < n_iter; ++i) {
         /* m = m1*m2*m3 -> tmp = m1*m2, m = tmp*m3 */
         //mult(m1,m2,tmp); mult(tmp,m3,m);
