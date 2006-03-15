d4f24484889173024097d889320ed3e6f755deac
dnave <demianmnave@users.noreply.github.com>
2006-03-15 17:49:25 +0000

    Added a header that pulls all of CML into the global namespace for simplicity.

---
+++ cml/cml.h
@@ -0,0 +1,21 @@
+/* -*- C++ -*- ------------------------------------------------------------
+ @@COPYRIGHT@@
+ *-----------------------------------------------------------------------*/
+/** @file
+ *  @brief Include all CML headers, and pull CML into the global namespace.
+ */
+
+#ifndef cml_h
+#define cml_h
+
+#include <cml/vector.h>
+#include <cml/matrix.h>
+using namespace cml;
+using namespace cml::vector_ops;
+using namespace cml::matvec_ops;
+using namespace cml::matrix_ops;
+
+#endif
+
+// -------------------------------------------------------------------------
+// vim:ft=cpp
