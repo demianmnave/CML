6bdfbbcf880606b1c70438a5b92df8058a06a5b6
jesse-k <jyk2048@yahoo.com>
2007-03-08 01:24:13 +0000

    *** empty log message ***

--- cml/mathlib/epsilon.h
+++ cml/mathlib/epsilon.h
@@ -17,18 +17,8 @@ namespace cml {
 
 /* @todo: epsilon and tolerance handling.
  *
- * Note: Handling of epsilons, tolerances, and degenerate and corner cases
- * is currently an unaddressed issue in the CML, but is at the top of the
- * to-do list.
- *
- * Currently, functions for which an epsilon is applicable take an epsilon
- * value as the last argument. For now, this always defaults to the arbitrary
- * placeholder value, but can of course be specified by the user instead.
- *
- * At some point (soon, I hope) the epsilon class will be filled out to
- * provide meaningful and reasonable epsilons for different situations and
- * scalar types. It may also be possible to set the default values for various
- * purposes (e.g. vector normalization, matrix singularity check, and so on).
+ * @note This is a placeholder for a more sophisticated epsilon/tolerance
+ * system.
  */
 
 template < typename Real >
