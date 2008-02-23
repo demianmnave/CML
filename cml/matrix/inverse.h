c3128ac6c581cc46281a5385902e444b09c03734
jesse-k <jyk2048@yahoo.com>
2008-02-23 01:22:04 +0000

    Added inverse determinant optimization. Removed 'force_NxN' debug code.

--- cml/matrix/inverse.h
+++ cml/matrix/inverse.h
@@ -34,18 +34,20 @@ struct inverse_f<MatT,2>
 {
     typename MatT::temporary_type operator()(const MatT& M) const
     {
+        typedef typename MatT::temporary_type temporary_type;
+        typedef typename temporary_type::value_type value_type;
+
         /* Matrix containing the inverse: */
-        typename MatT::temporary_type Z;
+        temporary_type Z;
         cml::et::detail::Resize(Z,2,2);
 
         /* Compute determinant and inverse: */
-        typename MatT::value_type D = M(0,0)*M(1,1) - M(0,1)*M(1,0);
-        Z(0,0) =   M(1,1)/D; Z(0,1) = - M(0,1)/D;
-        Z(1,0) = - M(1,0)/D; Z(1,1) =   M(0,0)/D;
+        value_type D = value_type(1) / (M(0,0)*M(1,1) - M(0,1)*M(1,0));
+        Z(0,0) =   M(1,1)*D; Z(0,1) = - M(0,1)*D;
+        Z(1,0) = - M(1,0)*D; Z(1,1) =   M(0,0)*D;
 
         return Z;
     }
-
 };
 
 /* 3x3 inverse.  Despite being marked for fixed_size matrices, this can
@@ -77,16 +79,17 @@ struct inverse_f<MatT,3>
         value_type m_22 = M(0,0)*M(1,1) - M(0,1)*M(1,0);
 
         /* Compute determinant from the minors: */
-        value_type D = M(0,0)*m_00 + M(0,1)*m_01 + M(0,2)*m_02;
+        value_type D =
+            value_type(1) / (M(0,0)*m_00 + M(0,1)*m_01 + M(0,2)*m_02);
 
         /* Matrix containing the inverse: */
         typename MatT::temporary_type Z;
         cml::et::detail::Resize(Z,3,3);
 
         /* Assign the inverse as (1/D) * (cofactor matrix)^T: */
-        Z(0,0) = m_00/D;  Z(0,1) = m_10/D;  Z(0,2) = m_20/D;
-        Z(1,0) = m_01/D;  Z(1,1) = m_11/D;  Z(1,2) = m_21/D;
-        Z(2,0) = m_02/D;  Z(2,1) = m_12/D;  Z(2,2) = m_22/D;
+        Z(0,0) = m_00*D;  Z(0,1) = m_10*D;  Z(0,2) = m_20*D;
+        Z(1,0) = m_01*D;  Z(1,1) = m_11*D;  Z(1,2) = m_21*D;
+        Z(2,0) = m_02*D;  Z(2,1) = m_12*D;  Z(2,2) = m_22*D;
 
         return Z;
     }
@@ -192,11 +195,12 @@ struct inverse_f<MatT,4>
         typename MatT::temporary_type Z;
         cml::et::detail::Resize(Z,4,4);
 
-        value_type D = M(0,0)*d00 - M(0,1)*d01 + M(0,2)*d02 - M(0,3)*d03;
-        Z(0,0) = +d00/D; Z(0,1) = -d10/D; Z(0,2) = +d20/D; Z(0,3) = -d30/D;
-        Z(1,0) = -d01/D; Z(1,1) = +d11/D; Z(1,2) = -d21/D; Z(1,3) = +d31/D;
-        Z(2,0) = +d02/D; Z(2,1) = -d12/D; Z(2,2) = +d22/D; Z(2,3) = -d32/D;
-        Z(3,0) = -d03/D; Z(3,1) = +d13/D; Z(3,2) = -d23/D; Z(3,3) = +d33/D;
+        value_type D = value_type(1) /
+            (M(0,0)*d00 - M(0,1)*d01 + M(0,2)*d02 - M(0,3)*d03);
+        Z(0,0) = +d00*D; Z(0,1) = -d10*D; Z(0,2) = +d20*D; Z(0,3) = -d30*D;
+        Z(1,0) = -d01*D; Z(1,1) = +d11*D; Z(1,2) = -d21*D; Z(1,3) = +d31*D;
+        Z(2,0) = +d02*D; Z(2,1) = -d12*D; Z(2,2) = +d22*D; Z(2,3) = -d32*D;
+        Z(3,0) = -d03*D; Z(3,1) = +d13*D; Z(3,2) = -d23*D; Z(3,3) = +d33*D;
 
         return Z;
     }
@@ -365,38 +369,50 @@ struct inverse_f
  * be taken out.
  */
 
+/* Note: Commenting the force_NxN stuff out, but leaving the code here in
+ * case we need to do more testing in the future.
+ */
+
 /* Generator for the inverse functional for fixed-size matrices: */
 template<typename MatT> typename MatT::temporary_type
-inverse(const MatT& M, fixed_size_tag, bool force_NxN)
+inverse(const MatT& M, fixed_size_tag/*, bool force_NxN*/)
 {
     /* Require a square matrix: */
     cml::et::CheckedSquare(M, fixed_size_tag());
     
+    /*
     if (force_NxN) {
         return inverse_f<MatT,0>()(M);
     } else {
-        return inverse_f<MatT,MatT::array_rows>()(M);
+    */
+    return inverse_f<MatT,MatT::array_rows>()(M);
+    /*
     }
+    */
 }
 
 /* Generator for the inverse functional for dynamic-size matrices: */
 template<typename MatT> typename MatT::temporary_type
-inverse(const MatT& M, dynamic_size_tag, bool force_NxN)
+inverse(const MatT& M, dynamic_size_tag/*, bool force_NxN*/)
 {
     /* Require a square matrix: */
     cml::et::CheckedSquare(M, dynamic_size_tag());
     
+    /*
     if (force_NxN) { 
         return inverse_f<MatT,0>()(M);
     } else {
-        /* Dispatch based upon the matrix dimension: */
-        switch(M.rows()) {
-            case 2:  return inverse_f<MatT,2>()(M);     //   2x2
-            case 3:  return inverse_f<MatT,3>()(M);     //   3x3
-            case 4:  return inverse_f<MatT,4>()(M);     //   4x4
-            default: return inverse_f<MatT,0>()(M);     // > 4x4 (or 1x1)
-        }
+    */
+    /* Dispatch based upon the matrix dimension: */
+    switch(M.rows()) {
+        case 2:  return inverse_f<MatT,2>()(M);     //   2x2
+        case 3:  return inverse_f<MatT,3>()(M);     //   3x3
+        case 4:  return inverse_f<MatT,4>()(M);     //   4x4
+        default: return inverse_f<MatT,0>()(M);     // > 4x4 (or 1x1)
     }
+    /*
+    }
+    */
 }
 
 } // namespace detail
@@ -404,19 +420,19 @@ inverse(const MatT& M, dynamic_size_tag, bool force_NxN)
 /** Inverse of a matrix. */
 template<typename E, class AT, typename BO, typename L> inline
 typename matrix<E,AT,BO,L>::temporary_type
-inverse(const matrix<E,AT,BO,L>& M, bool force_NxN = false)
+inverse(const matrix<E,AT,BO,L>& M/*, bool force_NxN = false*/)
 {
     typedef typename matrix<E,AT,BO,L>::size_tag size_tag;
-    return detail::inverse(M,size_tag(),force_NxN);
+    return detail::inverse(M,size_tag()/*,force_NxN*/);
 }
 
 /** Inverse of a matrix expression. */
 template<typename XprT> inline
 typename et::MatrixXpr<XprT>::temporary_type
-inverse(const et::MatrixXpr<XprT>& e, bool force_NxN = false)
+inverse(const et::MatrixXpr<XprT>& e/*, bool force_NxN = false*/)
 {
     typedef typename et::MatrixXpr<XprT>::size_tag size_tag;
-    return detail::inverse(e,size_tag(),force_NxN);
+    return detail::inverse(e,size_tag()/*,force_NxN*/);
 }
 
 } // namespace cml
