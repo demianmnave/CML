1f5dec9495b7d0fda66af1fe178aee686e662631
dnave <demian@thenaves.org>
2016-09-03 13:40:10 -0400

    Changes:
    - Added missing basis.h include in matrix/functions.h.

--- cml/matrix/functions.h
+++ cml/matrix/functions.h
@@ -10,6 +10,7 @@
 #define	cml_matrix_functions_h
 
 #include <cml/matrix/row_col.h>
+#include <cml/matrix/basis.h>
 #include <cml/matrix/transpose.h>
 #include <cml/matrix/determinant.h>
 #include <cml/matrix/trace.h>
