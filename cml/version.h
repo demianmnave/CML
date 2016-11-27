6489de24404bb1ab014699143c95a032e5524653
dnave <demian@thenaves.org>
2016-11-27 09:09:35 -0500

    Changes:
    - Bumped CML2 version to 2.0.3.

--- cml/version.h
+++ cml/version.h
@@ -10,10 +10,10 @@
 #define	cml_version_h
 
 /* Current CML version: */
-#define CML_VERSION			200002U
+#define CML_VERSION			200003U
 
 /* Current CML version as a string: */
-#define CML_VERSION_STRING		"2.0.2"
+#define CML_VERSION_STRING		"2.0.3"
 
 #endif
 
