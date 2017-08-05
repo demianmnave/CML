84f5ec77d9549a2129707990d099bd12c4041dab
dnave <demian@thenaves.org>
2017-08-05 11:28:16 -0400

    Moved cml::detail::hash_combine_impl into a separate header, and modified CMakeLists.txt to add "/permissive-" when building with VS2017.

--- cml/util/vector_hash.h
+++ cml/util/vector_hash.h
@@ -9,66 +9,10 @@
 #ifndef cml_util_vector_hash_h
 #define cml_util_vector_hash_h
 
-#include <cstdint>
-#include <functional>
 #include <cml/common/mpl/int_c.h>
+#include <cml/common/hash.h>
 #include <cml/vector/vector.h>
 #include <cml/vector/traits.h>
-
-namespace cml {
-namespace detail {
-
-/* Adapted mrom boost/functional/hash/hash.hpp> */
-#if defined(_MSC_VER)
-#define _HASH_ROTL32(x, r) _rotl(x,r)
-#else
-#define _HASH_ROTL32(x, r) ((x << r) | (x >> (32 - r)))
-#endif
-
-template<typename U32> inline void
-hash_combine_impl(U32& h1, U32 k1, cml::int_c<4>)
-{
-  const auto c1 = UINT32_C(0xcc9e2d51);
-  const auto c2 = UINT32_C(0x1b873593);
-
-  k1 *= c1;
-  k1 = _HASH_ROTL32(k1, 15);
-  k1 *= c2;
-
-  h1 ^= k1;
-  h1 = _HASH_ROTL32(h1, 13);
-  h1 = h1 * 5 + 0xe6546b64;
-}
-
-#undef _CML_HASH_ROTL32
-
-template<typename U64> inline void
-hash_combine_impl(U64& h, U64 k, cml::int_c<8>)
-{
-  const auto m = UINT64_C(0xc6a4a7935bd1e995);
-  const auto r = 47;
-
-  k *= m;
-  k ^= k >> r;
-  k *= m;
-
-  h ^= k;
-  h *= m;
-
-  // Completely arbitrary number, to prevent 0's
-  // from hashing to 0.
-  h += UINT64_C(0xe6546b64);
-}
-
-
-template<typename UInt> inline void
-hash_combine(UInt& h, UInt k)
-{
-  hash_combine_impl(h, k, cml::int_c<sizeof(UInt)>());
-}
-
-}
-}
 
 /* Need specialization in std: */
 namespace std {
