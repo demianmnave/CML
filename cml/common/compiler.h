d35184412fa6db09aeba667615c900e912f5b955
dnave <demian@thenaves.org>
2018-03-28 10:19:53 -0400

    Initial use of _cpp_<*> macros for testing C++ features under Clang (#13).
    - Tested locally using Clang6 from Visual Studio 2017 (with LLVM-vs2017 target integration).

--- cml/common/compiler.h
+++ cml/common/compiler.h
@@ -11,7 +11,6 @@
 
 #if defined(__GNUC__) && (__GNUC__ > 4 \
   || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 9)))
-/* g++ support for C++11 features used by CML: */
 
 /* N2439 move semantics for *this, used, for example, to efficiently return
  * an expression node from a class method. Without this, a temporary is
@@ -33,8 +32,54 @@
 /* This dummy qualifier is not needed for gcc: */
 #  define CML_DUMMY_TYPENAME
 
+#elif defined(__clang__)
+/* Note: checking for Clang BEFORE checking for Visual Studio, since clang-cl
+ * defines _MSC_VER.
+ */
+
+# ifdef __cpp_ref_qualifiers
+/* N2439 move semantics for *this, used, for example, to efficiently return
+ * an expression node from a class method. Without this, a temporary is
+ * returned instead:
+ */
+#  define CML_HAS_RVALUE_REFERENCE_FROM_THIS
+
+/* Trailing method type for ref from *this (to disambiguate overloads using
+ * rvalue reference from this):
+ */
+#  define __CML_REF		&
+# else
+#  define __CML_REF
+# endif
+
+
+/* Constexpr support: */
+# ifdef __cpp_constexpr
+#  define CML_CONSTEXPR constexpr
+# else
+#  define CML_CONSTEXPR inline
+# endif
+
+
+/* Structured bindings: */
+# ifdef __cpp_structured_bindings
+#  define CML_HAS_STRUCTURED_BINDINGS
+# endif
+
+
+/* Defaulted move constructor: */
+# if ((__clang_major__ >= 4) \
+  || ((__clang_major__ >= 3) && (__clang_minor__ >= 5))) \
+  || (__apple_build_version__ >= 7000000)
+
+#  define CML_HAS_DEFAULTED_MOVE_CONSTRUCTOR
+# endif
+
+
+/* This dummy qualifier is not needed for Clang: */
+#  define CML_DUMMY_TYPENAME
+
 #elif defined(_MSC_VER)
-/* VC++ support for C++11 features used by CML: */
 
 # if _MSC_VER < 1900
 
@@ -78,71 +123,7 @@
 
 # endif
 
-/* C++17 features: */
 # if (_MSC_VER >= 1911) && (_MSVC_LANG >= 201703L)
-
-/* Enable structured bindings: */
-#  define CML_HAS_STRUCTURED_BINDINGS
-
-# endif
-
-#elif defined(__clang__) && ((__clang_major__ >= 4) \
-  || ((__clang_major__ >= 3) && (__clang_minor__ >= 5)))
-/* clang support for C++11 features used by CML: */
-
-/* N2439 move semantics for *this, used, for example, to efficiently return
- * an expression node from a class method. Without this, a temporary is
- * returned instead:
- */
-# define CML_HAS_RVALUE_REFERENCE_FROM_THIS
-
-/* Trailing method type for ref from *this (to disambiguate overloads using
- * rvalue reference from this):
- */
-# define __CML_REF		&
-
-/* Defaulted move constructor supported: */
-# define CML_HAS_DEFAULTED_MOVE_CONSTRUCTOR
-
-/* Constexpr support: */
-# define CML_CONSTEXPR constexpr
-
-/* This dummy qualifier is not needed for Clang: */
-#  define CML_DUMMY_TYPENAME
-
-/* C++17 features: */
-# if __clang_major__ >= 4
-
-/* Enable structured bindings: */
-#  define CML_HAS_STRUCTURED_BINDINGS
-
-# endif
-
-#elif defined(__apple_build_version__) && (__apple_build_version__ >= 7000000)
-/* AppleClang support for C++11 features used by CML: */
-
-/* N2439 move semantics for *this, used, for example, to efficiently return
- * an expression node from a class method. Without this, a temporary is
- * returned instead:
- */
-# define CML_HAS_RVALUE_REFERENCE_FROM_THIS
-
-/* Trailing method type for ref from *this (to disambiguate overloads using
- * rvalue reference from this):
- */
-# define __CML_REF		&
-
-/* Defaulted move constructor supported: */
-# define CML_HAS_DEFAULTED_MOVE_CONSTRUCTOR
-
-/* Constexpr support: */
-# define CML_CONSTEXPR constexpr
-
-/* This dummy qualifier is not needed for Clang: */
-#  define CML_DUMMY_TYPENAME
-
-/* C++17 features: */
-# if __apple_build_version__ >= 9000000
 
 /* Enable structured bindings: */
 #  define CML_HAS_STRUCTURED_BINDINGS
@@ -157,6 +138,9 @@
 
 /* Assume no rvalue reference from this: */
 // # define CML_HAS_RVALUE_REFERENCE_FROM_THIS
+
+/* Assume no structured bindings: */
+// # define CML_HAS_STRUCTURED_BINDINGS
 
 /* Assume no disambiguation needed: */
 # define __CML_REF
