623335e357d906365bb703a4210876ba10227963
dnave <demian@thenaves.org>
2018-03-28 16:59:31 -0400

    Forgot to update version.h.

--- cml/version.h
+++ cml/version.h
@@ -10,10 +10,10 @@
 #define	cml_version_h
 
 /* Current CML version: */
-#define CML_VERSION			200100U
+#define CML_VERSION			200101U
 
 /* Current CML version as a string: */
-#define CML_VERSION_STRING		"2.1.0"
+#define CML_VERSION_STRING		"2.1.1"
 
 #endif
 
