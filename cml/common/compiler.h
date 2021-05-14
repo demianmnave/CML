bd572510cd45425af505910b0a47c8807a476e2d
demianmnave <demian@thenaves.org>
2021-05-14 16:00:21 -0400

    Tidy up the CMake build, add an installation option, tidy up AppVeyor using the Ninja CMake generator, and fix _source_group(...)_ usage
    Closes #27, #29, #30, #31

--- cml/common/compiler.h
+++ cml/common/compiler.h
@@ -25,36 +25,4 @@
 # define CML_HAS_STRUCTURED_BINDINGS
 #endif
 
-#if defined(__GNUC__) && (__GNUC__ > 4 \
-  || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 9)))
-
-/* Defaulted move constructor supported: */
-# define CML_HAS_DEFAULTED_MOVE_CONSTRUCTOR
-
-#elif defined(__clang__)
-/* Note: checking for Clang BEFORE checking for Visual Studio, since clang-cl
- * defines _MSC_VER.
- */
-
-/* Defaulted move constructor: */
-# if ((__clang_major__ >= 4) \
-  || ((__clang_major__ >= 3) && (__clang_minor__ >= 5))) \
-  || (__apple_build_version__ >= 7000000)
-
-#  define CML_HAS_DEFAULTED_MOVE_CONSTRUCTOR
-# endif
-
-#elif defined(_MSC_VER)
-
-# if(_MSC_VER < 1914)
-/* Apparently, VS2015 does not allow a parameter pack expansion to target non-
- * packed parameters.
- */
-#  define CML_HAS_MSVC_WONKY_PARAMETER_PACK
-# endif
-
-#else
-#error "Unsupported compiler."
-#endif
-
 #endif
