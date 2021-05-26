70b1fdd202ebd23ebc68571944bd5c5c9bae646f
demianmnave <demian@thenaves.org>
2021-05-26 16:05:47 -0400

    Add CML type system specializations for a 4x4 C-array (for the timing code only)

--- tests/timing/cmatrix.h
+++ tests/timing/cmatrix.h
@@ -7,4 +7,21 @@
 
 #pragma once
 
+#include <cml/common/traits.h>
+
 using matrix44d = double[4][4];
+
+/* Need some specializations for CML: */
+namespace cml {
+
+struct matrix44d_traits
+{
+  using value_type = double;
+};
+
+template<> struct traits_of<matrix44d>
+{
+  using type = matrix44d_traits;
+};
+
+} // namespace cml
