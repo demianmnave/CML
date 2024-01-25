27c5b3d7a0f593b6dd299dc235e929d279256595
demianmnave <demian@thenaves.org>
2024-01-24 23:06:19 -0500

    Update the version to 2.2.2

--- cml/version.h
+++ cml/version.h
@@ -10,9 +10,9 @@
 #define	cml_version_h
 
 /* Current CML version: */
-#define CML_VERSION			200200U
+#define CML_VERSION			200202U
 
 /* Current CML version as a string: */
-#define CML_VERSION_STRING		"2.2.0"
+#define CML_VERSION_STRING		"2.2.2"
 
 #endif
