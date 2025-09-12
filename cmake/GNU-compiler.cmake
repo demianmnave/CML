b8651f8b6aff8607892adc9cd6b0fd6e8e5fbc4f
Demian M. Nave <dnave@navehaus.com>
2025-09-12 11:42:59 -0400

    Add a GNU compiler configuration

---
+++ cmake/GNU-compiler.cmake
@@ -0,0 +1,36 @@
+# GCC compiler defaults.
+
+include_guard()
+
+message(STATUS "${PROJECT_NAME}: configuring for ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
+
+if(BUILD_STATIC_RUNTIME)
+  set(_cml_private_cxx_options -static)
+  if(NOT DARWIN)
+    # OS/X does not allow full static linking:
+    set(_cml_private_exe_link_options -static)
+  endif()
+endif()
+
+list(APPEND _cml_private_cxx_options
+  -pedantic      # Stricter compliance
+)
+if(PROJECT_IS_TOP_LEVEL)
+  list(APPEND _cml_private_cxx_options
+    -Wall        # All warnings
+    -Wextra
+  )
+endif()
+
+list(APPEND _cml_private_cxx_options_debug -g)
+list(APPEND _cml_private_cxx_options_release -O2)
+list(APPEND _cml_private_cxx_definitions -DNDEBUG)
+
+set(CML_TARGET_PROPERTIES ${_cml_target_properties})
+set(CML_PUBLIC_CXX_COMPILE_FEATURES ${CML_CXX_STD})
+set(CML_PUBLIC_CXX_DEFINITIONS ${_cml_public_cxx_definitions})
+set(CML_PRIVATE_CXX_DEFINITIONS ${_cml_private_cxx_definitions})
+set(CML_PRIVATE_CXX_OPTIONS ${_cml_private_cxx_options})
+set(CML_PRIVATE_CXX_OPTIONS_DEBUG ${_cml_private_cxx_options_debug})
+set(CML_PRIVATE_CXX_OPTIONS_RELEASE ${_cml_private_cxx_options_release})
+set(CML_PRIVATE_EXE_LINK_OPTIONS ${_cml_private_exe_link_options})
