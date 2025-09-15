e0af6f59823adbf709d220eee6e0f844fc64076a
Demian M. Nave <dnave@navehaus.com>
2025-09-15 11:43:02 -0400

    Work around strange initial config behavior of CMAKE_CXX_COMPILER_ARCHITECTURE_ID

--- cmake/host-functions.cmake
+++ cmake/host-functions.cmake
@@ -7,9 +7,9 @@ include_guard()
 # Parameters:
 #  OutArch (required) The variable to store the result to.
 function(cml_get_host_arch OutArch)
-  if(DEFINED CMAKE_CXX_COMPILER_ARCHITECTURE_ID)
+  if(NOT "${CMAKE_CXX_COMPILER_ARCHITECTURE_ID}" STREQUAL "")
     set(${OutArch} ${CMAKE_CXX_COMPILER_ARCHITECTURE_ID} PARENT_SCOPE)
-  elseif(DEFINED CMAKE_HOST_SYSTEM_PROCESSOR)
+  elseif(NOT "${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "")
     set(${OutArch} ${CMAKE_HOST_SYSTEM_PROCESSOR} PARENT_SCOPE)
   else()
     message(FATAL_ERROR "Cannot determine host architecture")
