e5e67b979427f390300dc6534e312f82f78c7d48
Demian M. Nave <dnave@navehaus.com>
2025-12-04 17:06:21 -0500

    Refactor compiler configuration into functions

--- cmake/Clang-compiler.cmake
+++ cmake/Clang-compiler.cmake
@@ -3,97 +3,148 @@
 include_guard()
 include(host-functions)
 
-message(STATUS "${PROJECT_NAME}: configuring for ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION} (${CMAKE_CXX_COMPILER_FRONTEND_VARIANT})")
+function(cml_configure_compiler)
+  # The variables to export to the caller's scope:
+  set(_defined)
 
-if(MSVC)
-  # Force include paths to be set in .vcxproj, not on the compiler command line
-  # to avoid IntelliSense errors:
-  #
-  # See also:
-  #  https://discourse.cmake.org/t/how-to-override-the-cmake-include-system-flag-lang/3615
-  #  https://discourse.cmake.org/t/imported-projects-dont-resolve-include-paths-in-intellisense/9185
-  unset(CMAKE_INCLUDE_SYSTEM_FLAG_CXX)
+  # Common configuration:
+  set(_cml_private_cxx_features ${CML_CXX_STD})
 
-  # Added to CML_TARGET_PROPERTIES:
-  if(NOT DEFINED MSVC_RUNTIME_LIBRARY)
-    if(BUILD_STATIC_RUNTIME)
-      set(MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+  set(CML_PRIVATE_CXX_FEATURES ${_cml_private_cxx_features})
+  list(APPEND _defined CML_PRIVATE_CXX_FEATURES)
+
+  if(MSVC)
+    # Force include paths to be set in .vcxproj, not on the compiler command line
+    # to avoid IntelliSense errors:
+    #
+    # See also:
+    #  https://discourse.cmake.org/t/how-to-override-the-cmake-include-system-flag-lang/3615
+    #  https://discourse.cmake.org/t/imported-projects-dont-resolve-include-paths-in-intellisense/9185
+    unset(CMAKE_INCLUDE_SYSTEM_FLAG_CXX PARENT_SCOPE)
+
+    # Added to CML_TARGET_PROPERTIES:
+    if(NOT DEFINED MSVC_RUNTIME_LIBRARY)
+      if(BUILD_STATIC_RUNTIME)
+        set(MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+      else()
+        set(MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+      endif() 
+    endif()
+
+    set(_cml_target_properties MSVC_RUNTIME_LIBRARY ${MSVC_RUNTIME_LIBRARY})
+    set(_cml_private_cxx_options
+      /permissive-   # Strict(er) standards conformance
+      /W4            # Most warnings
+      /FAs
+    )
+    set(_cml_private_exe_link_options /NOIMPLIB)
+
+
+    #>> Handle SIMD selection:
+    #TODO move this to a separate function/macro?
+    if(DEFINED CML_SIMD)
+      set(_cml_simd ${CML_SIMD})
     else()
-      set(MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
-    endif() 
-  endif()
+      cml_get_host_arch(_arch)
+      if(${_arch} STREQUAL "x64")
+        set(_cml_simd "sse4.2")
+      endif()
+    endif()
 
-  # Enforce the default for all projects:
-  if(NOT DEFINED CMAKE_MSVC_RUNTIME_LIBRARY)
-    set(CMAKE_MSVC_RUNTIME_LIBRARY ${MSVC_RUNTIME_LIBRARY})
-  endif()
-
-  set(_cml_target_properties
-    MSVC_RUNTIME_LIBRARY ${MSVC_RUNTIME_LIBRARY}
-  )
-
-  set(_cml_public_cxx_definitions
-    _UNICODE
-    UNICODE
-    NOMINMAX
-    WIN32_LEAN_AND_MEAN
-    _WIN32_WINNT=0x0A00
-  )
-
-  set(_cml_private_cxx_options
-    /permissive-   # Strict(er) standards conformance
-  )
-  if(PROJECT_IS_TOP_LEVEL)
-    list(APPEND _cml_private_cxx_options
-      /W4          # Most warnings
-      /MP
+    set(_cml_simd_map)
+    list(APPEND _cml_simd_map
+      "sse4.2" "sse4.2"
+      "avx2" "avx2"
     )
 
-    cml_get_host_arch(_arch)
-    if(${_arch} STREQUAL "x64")
-      list(APPEND _cml_private_cxx_options /arch:AVX /clang:-fvectorize)
+    list(FIND _cml_simd_map ${_cml_simd} _idx)
+    if(_idx EQUAL "-1")
+      message(FATAL_ERROR "Unknown SIMD type ${_cml_simd}")
+    else()
+      math(EXPR _idx "${_idx} + 1")
+      list(GET _cml_simd_map ${_idx} _cml_simd_opt)
     endif()
-  endif()
+    # list(APPEND _cml_private_cxx_options_release "/arch:${_cml_simd_opt}" "/clang:-fvectorize" "/clang:-fassociative-math")
+    list(APPEND _cml_private_cxx_options_release "/clang:-m${_cml_simd_opt}" "/clang:-fvectorize" "/clang:-fassociative-math")
+    #<< Handle SIMD selection:
 
-  set(_cml_private_exe_link_options /NOIMPLIB)
-else()
-  if(BUILD_STATIC_RUNTIME)
-    set(_cml_private_cxx_options -static)
-    if(WIN32)
-      # Must be defined, otherwise CMake passes -D_DLL to clang on Windows:
-      set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-      set(_cml_target_properties
-        MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>"
-        )
-    elseif(NOT DARWIN)
-      # OS/X does not allow full static linking:
-      set(_cml_private_exe_link_options -static)
+
+    set(CML_TARGET_PROPERTIES ${_cml_target_properties})
+    list(APPEND _defined CML_TARGET_PROPERTIES)
+
+    set(CML_PRIVATE_CXX_OPTIONS ${_cml_private_cxx_options})
+    list(APPEND _defined CML_PRIVATE_CXX_OPTIONS)
+
+    set(CML_PRIVATE_CXX_OPTIONS_RELEASE ${_cml_private_cxx_options_release})
+    list(APPEND _defined CML_PRIVATE_CXX_OPTIONS_RELEASE)
+
+    set(CML_PRIVATE_EXE_LINK_OPTIONS ${_cml_private_exe_link_options})
+    list(APPEND _defined CML_PRIVATE_EXE_LINK_OPTIONS)
+
+    set(CML_SIMD ${_cml_simd})
+    list(APPEND _defined CML_SIMD)
+  else()
+    if(BUILD_STATIC_RUNTIME)
+      set(_cml_private_cxx_options -static)
+      if(WIN32)
+        # Must be defined, otherwise CMake passes -D_DLL to clang on Windows:
+        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+        set(_cml_target_properties
+          MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>"
+          )
+      elseif(NOT DARWIN)
+        # OS/X does not allow full static linking:
+        set(_cml_private_exe_link_options -static)
+      endif()
     endif()
-  endif()
 
-  list(APPEND _cml_private_cxx_options
-    -pedantic      # Stricter compliance
-  )
-  if(PROJECT_IS_TOP_LEVEL)
     list(APPEND _cml_private_cxx_options
-      -Wall        # All warnings
+      -pedantic      # Stricter compliance
+      -Wall          # All warnings
       -Wextra
     )
+    list(APPEND _cml_private_cxx_options_release -O2)
 
-    cml_get_host_arch(_arch)
-    if(${_arch} STREQUAL "x64")
-      list(APPEND _cml_private_cxx_options -march=native -mavx -fvectorize -fassociative-math)
+    #>> Handle SIMD selection:
+    #TODO move this to a separate function/macro?
+    if(NOT DEFINED CML_SIMD)
+      cml_get_host_arch(_arch)
+      if(${_arch} STREQUAL "x64")
+        set(_cml_simd sse2)
+      endif()
     endif()
+
+    set(_cml_simd_map)
+    list(APPEND _cml_simd_map
+      "sse2" "sse2"
+      "avx2" "avx2"
+    )
+
+    list(FIND _cml_simd_map ${_cml_simd} _idx)
+    if(_idx EQUAL "-1")
+      message(FATAL_ERROR "Unknown SIMD type ${_cml_simd}")
+    else()
+      math(EXPR _idx "${_idx} + 1")
+      list(GET _cml_simd_map ${_idx} _cml_simd_opt)
+    endif()
+    list(APPEND _cml_private_cxx_options_release "-m${_cml_simd_opt}" "-fvectorize" "-fassociative-math")
+    #<< Handle SIMD selection:
+
+
+    set(CML_TARGET_PROPERTIES ${_cml_target_properties})
+    list(APPEND _defined CML_TARGET_PROPERTIES)
+
+    set(CML_PRIVATE_CXX_OPTIONS ${_cml_private_cxx_options})
+    list(APPEND _defined CML_PRIVATE_CXX_OPTIONS)
+
+    set(CML_PRIVATE_CXX_OPTIONS_RELEASE ${_cml_private_cxx_options_release})
+    list(APPEND _defined CML_PRIVATE_CXX_OPTIONS_RELEASE)
+
+    set(CML_PRIVATE_EXE_LINK_OPTIONS ${_cml_private_exe_link_options})
+    list(APPEND _defined CML_PRIVATE_EXE_LINK_OPTIONS)
   endif()
 
-  list(APPEND _cml_private_cxx_options_release -O2)
-endif()
-
-set(CML_TARGET_PROPERTIES ${_cml_target_properties})
-set(CML_PUBLIC_CXX_COMPILE_FEATURES ${CML_CXX_STD})
-set(CML_PUBLIC_CXX_DEFINITIONS ${_cml_public_cxx_definitions})
-set(CML_PRIVATE_CXX_DEFINITIONS ${_cml_private_cxx_definitions})
-set(CML_PRIVATE_CXX_OPTIONS ${_cml_private_cxx_options})
-set(CML_PRIVATE_CXX_OPTIONS_DEBUG ${_cml_private_cxx_options_debug})
-set(CML_PRIVATE_CXX_OPTIONS_RELEASE ${_cml_private_cxx_options_release})
-set(CML_PRIVATE_EXE_LINK_OPTIONS ${_cml_private_exe_link_options})
+  foreach(_list IN LISTS _defined)
+    set(${_list} ${${_list}} PARENT_SCOPE)
+  endforeach()
+endfunction()
--- cmake/MSVC-compiler.cmake
+++ cmake/MSVC-compiler.cmake
@@ -1,11 +1,9 @@
-# Clang compiler defaults.
+# MSVC compiler defaults.
 
 include_guard()
 include(host-functions)
 
-message(STATUS "${PROJECT_NAME}: configuring for ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION} (${CMAKE_CXX_COMPILER_FRONTEND_VARIANT})")
-
-if(MSVC)
+function(cml_configure_compiler)
   # Added to CML_TARGET_PROPERTIES:
   if(NOT DEFINED MSVC_RUNTIME_LIBRARY)
     if(BUILD_STATIC_RUNTIME)
@@ -15,52 +13,72 @@ if(MSVC)
     endif() 
   endif()
 
-  # Enforce the default for all projects:
-  if(NOT DEFINED CMAKE_MSVC_RUNTIME_LIBRARY)
-    set(CMAKE_MSVC_RUNTIME_LIBRARY ${MSVC_RUNTIME_LIBRARY})
-  endif()
-
-  set(_cml_target_properties
-    MSVC_RUNTIME_LIBRARY ${MSVC_RUNTIME_LIBRARY}
-  )
-
-  set(_cml_public_cxx_definitions
-    _UNICODE
-    UNICODE
-    NOMINMAX
-    WIN32_LEAN_AND_MEAN
-    _WIN32_WINNT=0x0A00
-  )
-
+  set(_cml_target_properties MSVC_RUNTIME_LIBRARY ${MSVC_RUNTIME_LIBRARY})
+  set(_cml_private_cxx_features ${CML_CXX_STD})
   set(_cml_private_cxx_options
     /permissive-   # Strict(er) standards conformance
     /GL            # Link-time code generation
+    /W4            # Most warnings
+    /diagnostics:caret
+    /MP
+    /EHsc
+    /bigobj
+    /FAs
   )
+  set(_cml_private_exe_link_options /NOIMPLIB /NOEXP /LTCG)
 
-  if(PROJECT_IS_TOP_LEVEL)
-    list(APPEND _cml_private_cxx_options
-      /W4          # Most warnings
-      /diagnostics:caret
-      /MP
-      /EHsc
-      /bigobj
-      /utf-8
-    )
 
+  #>> Handle SIMD selection:
+  #TODO move this to a separate function/macro?
+  if(DEFINED CML_SIMD)
+    set(_cml_simd ${CML_SIMD})
+  else()
     cml_get_host_arch(_arch)
     if(${_arch} STREQUAL "x64")
-      list(APPEND _cml_private_cxx_options /arch:AVX2)
+      set(_cml_simd "sse4.2")
     endif()
   endif()
 
-  set(_cml_private_exe_link_options /NOIMPLIB /NOEXP /LTCG)
-endif()
+  # Note that the compiler default is "SSE2" for MSVC-x64, and it enables SSE4.2:
+  set(_cml_simd_map)
+  list(APPEND _cml_simd_map
+    "sse4.2" "SSE2"
+    "avx2" "AVX2"
+  )
 
-set(CML_TARGET_PROPERTIES ${_cml_target_properties})
-set(CML_PUBLIC_CXX_COMPILE_FEATURES ${CML_CXX_STD})
-set(CML_PUBLIC_CXX_DEFINITIONS ${_cml_public_cxx_definitions})
-set(CML_PRIVATE_CXX_DEFINITIONS ${_cml_private_cxx_definitions})
-set(CML_PRIVATE_CXX_OPTIONS ${_cml_private_cxx_options})
-set(CML_PRIVATE_CXX_OPTIONS_DEBUG ${_cml_private_cxx_options_debug})
-set(CML_PRIVATE_CXX_OPTIONS_RELEASE ${_cml_private_cxx_options_release})
-set(CML_PRIVATE_EXE_LINK_OPTIONS ${_cml_private_exe_link_options})
+  list(FIND _cml_simd_map ${_cml_simd} _idx)
+  if(_idx EQUAL "-1")
+    message(FATAL_ERROR "Unknown SIMD type ${_cml_simd}")
+  else()
+    math(EXPR _idx "${_idx} + 1")
+    list(GET _cml_simd_map ${_idx} _cml_simd_opt)
+  endif()
+  list(APPEND _cml_private_cxx_options_release "/arch:${_cml_simd_opt}")
+  #<< Handle SIMD selection:
+
+
+  # The variables to export to the caller's scope:
+  set(_defined)
+
+  set(CML_TARGET_PROPERTIES ${_cml_target_properties})
+  list(APPEND _defined CML_TARGET_PROPERTIES)
+
+  set(CML_PRIVATE_CXX_FEATURES ${_cml_private_cxx_features})
+  list(APPEND _defined CML_PRIVATE_CXX_FEATURES)
+
+  set(CML_PRIVATE_CXX_OPTIONS ${_cml_private_cxx_options})
+  list(APPEND _defined CML_PRIVATE_CXX_OPTIONS)
+
+  set(CML_PRIVATE_CXX_OPTIONS_RELEASE ${_cml_private_cxx_options_release})
+  list(APPEND _defined CML_PRIVATE_CXX_OPTIONS_RELEASE)
+
+  set(CML_PRIVATE_EXE_LINK_OPTIONS ${_cml_private_exe_link_options})
+  list(APPEND _defined CML_PRIVATE_EXE_LINK_OPTIONS)
+
+  set(CML_SIMD ${_cml_simd})
+  list(APPEND _defined CML_SIMD)
+
+  foreach(_list IN LISTS _defined)
+    set(${_list} ${${_list}} PARENT_SCOPE)
+  endforeach()
+endfunction()
