97f6efae086104de04993cd8e4fb9b8c72f00e41
Demian M. Nave <dnave@navehaus.com>
2025-12-15 17:03:51 -0500

    Use a traits type for C matrices if available

--- cml/matrix/detail/get.h
+++ cml/matrix/detail/get.h
@@ -8,12 +8,13 @@
 #include <cml/matrix/writable_matrix.h>
 
 namespace cml::detail {
+
 /** Helper to return the passed-in value in response to a matrix index @c
  * (i,j).
  */
 template<class Other,
   typename = std::enable_if_t<!cml::is_matrix<Other>::value>>
-inline auto
+auto
 get(const Other& v, int, int) -> const Other&
 {
   return v;
@@ -21,31 +22,49 @@ get(const Other& v, int, int) -> const Other&
 
 /** Helper to return element @c (i,j) of @c array. */
 template<class Other, int Rows, int Cols>
-inline auto
+auto
 get(Other const (&array)[Rows][Cols], int i, int j) -> const Other&
 {
-  return array[i][j];
+  using array_type = cml::unqualified_type_t<decltype(array)>;
+  if constexpr(has_traits_v<array_type>) {
+    if constexpr(std::is_same_v<layout_tag_trait_of_t<array_type>, col_major>) {
+      return array[j][i];
+    } else {
+      return array[i][j];
+    }
+  } else {
+    return array[i][j];
+  }
 }
 
 /** Helper to return element @c (i,j) of @c array. */
 template<class Other, int Rows, int Cols>
-inline auto
+auto
 get(Other (&array)[Rows][Cols], int i, int j) -> Other&
 {
-  return array[i][j];
+  using array_type = cml::unqualified_type_t<decltype(array)>;
+  if constexpr(has_traits_v<array_type>) {
+    if constexpr(std::is_same_v<layout_tag_trait_of_t<array_type>, col_major>) {
+      return array[j][i];
+    } else {
+      return array[i][j];
+    }
+  } else {
+    return array[i][j];
+  }
 }
 
 /** Helper to return element @c (i,j) of @c sub. */
-template<class Sub> inline auto
-get(
-  const readable_matrix<Sub>& sub, int i, int j)
-  -> typename matrix_traits<Sub>::immutable_value
+template<class Sub>
+auto
+get(const readable_matrix<Sub>& sub, int i, int j) ->
+  typename matrix_traits<Sub>::immutable_value
 {
   return sub.get(i, j);
 }
 
 template<class Sub>
-inline auto
+auto
 get(writable_matrix<Sub>& sub, int i, int j) ->
   typename matrix_traits<Sub>::mutable_value
 {
